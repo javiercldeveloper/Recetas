7e6963bb585980562b518c84629643c74bedd21b
Twiggyns <seoprowannabe@gmail.com>
2018-11-25 22:09:11 +0100

    Add loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="loader">
+    <img src="../assets/img/loader.svg" alt="Cargando">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loader'
+}
+</script>
+
+<style scoped>
+.loader{
+  margin-top: 50px;
+  width: 100%;
+  text-align: center;
+}
+</style>
