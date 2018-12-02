dc4ab76b15dc0d8e48e64c26b6f502d5c68c7987
Twiggyns <seoprowannabe@gmail.com>
2018-12-02 13:37:08 +0100

    Fix Slider Css

--- src/components/TheSlider.vue
+++ src/components/TheSlider.vue
@@ -51,7 +51,7 @@ export default {
       text-align: center;
       display: flex;
       justify-items: center;
-      min-height: 200px;
+      min-height: 385px;
       margin-top: 50px;
   }
   .subslider{
@@ -59,4 +59,15 @@ export default {
       color: white;
       flex: 1;
   }
+
+  .slide-fade-enter-active {
+  transition: all .3s ease;
+  }
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to{
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
